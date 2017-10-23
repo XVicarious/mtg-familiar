6369d1b721fa1c3639d3ba76d7813cf741a609cc
Brian Maurer <bmaurer@xvss.net>
2017-10-23 12:46:56 -0400

    Update sumsrc.sh

--- .circleci/sumsrc.sh
+++ .circleci/sumsrc.sh
@@ -4,5 +4,5 @@ for f in $(find ~/repo/mobile/src/main/java -name "*.java")
 do
   md5sum $f >> md5sum.txt
 done;
-export $MD5SUM=$(md5sum md5sum.txt)
+export $MD5SUM="$(md5sum md5sum.txt)"
 echo $MD5SUM
