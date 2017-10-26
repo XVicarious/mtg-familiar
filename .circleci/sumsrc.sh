4a4b2f08dfc318dd6737fd0515be2999874dbcc9
Brian Maurer <bmaurer@xvss.net>
2017-10-26 11:14:54 -0400

    Delete sumsrc.sh

--- .circleci/sumsrc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-ls
-for f in $(find ~/repo/mobile/src/main/java -name "*.java")
-do
-  md5sum $f >> md5sum.txt
-done;
-export "$MD5SUM=${$(md5sum md5sum.txt | awk '{ print $1 }'):1}"
-echo $MD5SUM
